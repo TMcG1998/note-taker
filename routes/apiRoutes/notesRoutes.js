06b1f165b3b8f56b118e3ac8d2d4aed09f162d7d
Tommy McG <thomasjmcgovern98@gmail.com>
2022-04-29 19:24:41 -0400

    post and get working

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
+// Import notes functions
+const { createNewNote } = require('../../lib/notes');
 // Get JSON data
-const { notes } = require('../../db/db');
-// 
+const notes = require('../../db/db.json');
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/notes', (req, res) => {
+    res.json(notes);
+})
+
+// POST /api/notes should receive a new note to save on the request body, add it 
+// to the db.json file, and then return the new note to the client. You'll need 
+// to find a way to give each note a unique id when it's saved (look into npm 
+// packages that could do this for you).
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+})
+
+
+console.log(notes);
+module.exports = router;
